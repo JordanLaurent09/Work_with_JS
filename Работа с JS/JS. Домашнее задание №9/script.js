ace12e9e523c7a3b09079ce6bace14d779bee618
Сергей Ш <128598320+JordanLaurent09@users.noreply.github.com>
2024-07-10 17:36:23 +0300

    Update script.js

--- Работа с JS/JS. Домашнее задание №9/script.js
+++ Работа с JS/JS. Домашнее задание №9/script.js
@@ -4,10 +4,24 @@ let items = [
     [2, 1, 3, 3]
 ];
 
+let resultArr = items.reduce(function(accumulator, currentValue) {
+    currentValue.forEach(function(value) {
+        let isIncludes = false;
+        accumulator.forEach(function(innerValue) {
+            if (innerValue.includes(value) || innerValue.length == 0) {
+                isIncludes = true;
+                innerValue.push(value);
+            }
+        })
+        if (isIncludes == false) {
+            let newInnerValue = new Array();
+            newInnerValue.push(value);
+            accumulator.push(newInnerValue);
+        }
+    });
+    return accumulator;
+}, [[]]);
 
-let val = items.reduce(function(accumulator, currentValue) {
-    return accumulator.concat(currentValue).sort();
+resultArr.forEach(function(value) {
+    console.log(`Число ${value[0]} встречается в массиве ${value.length} раз(а).`);
 });
-
-console.log(val);
-
